a4b0d1d21c40143618d16d13a50e94ba3b62ba44
lhanusiak <Lukasz_Hanusiak@epam.com>
2021-10-15 13:53:40 +0200

    project import step

---
+++ src/main/java/pl/edu/pk/ztp/librarymonolith/LibraryMonolithApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.ztp.librarymonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pk/ztp/librarymonolith/LibraryMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pk.ztp.librarymonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
