d55c37a25e7c27d94707aa139ca5b6145476fc5e
lhanusiak <Lukasz_Hanusiak@epam.com>
2021-10-30 18:25:33 +0200

    README.md updates & docs

--- docs/V1_0__create_librarydb_schema.sql
+++ docs/V1_0__create_librarydb_schema.sql
@@ -30,7 +30,7 @@ INSERT INTO tbl_books(id, title, author, quantity) VALUES (5, 'Python. Zadania z
 ALTER TABLE tbl_books ALTER COLUMN id RESTART WITH 6;
 
 INSERT INTO tbl_rentals(id, userid_fk, bookid_fk, rental_date, return_date) VALUES (1001, 10, 5, '2021-10-01', '2021-10-14');
-INSERT INTO tbl_rentals(id, userid_fk, bookid_fk, rental_date, return_date) VALUES (1002, 10, 5, '2021-10-15', NULL);
-INSERT INTO tbl_rentals(id, userid_fk, bookid_fk, rental_date, return_date) VALUES (1003, 20, 5, '2021-10-16', NULL);
+INSERT INTO tbl_rentals(id, userid_fk, bookid_fk, rental_date, return_date) VALUES (1002, 10, 1, '2021-10-15', NULL);
+INSERT INTO tbl_rentals(id, userid_fk, bookid_fk, rental_date, return_date) VALUES (1003, 20, 1, '2021-10-16', NULL);
 ALTER TABLE tbl_rentals ALTER COLUMN id RESTART WITH 1004;
 
